a808bb5126e3c065eb4510c600f4dd93d93742fc
cjerzak <connor.jerzak@gmail.com>
2018-11-07 16:59:22 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -96,7 +96,7 @@
 #' @export 
 #' @import tensorflow
 readme <- function(dfm, labeledIndicator, categoryVec, 
-                   nboot = 10,  sgd_iters = 1000, sgd_momentum = .9, numProjections = 20, minBatch = 3, maxBatch = 20, mLearn= 0.01, dropout_rate = .5, kMatch = 3, minMatch = 5, nBoot_matching = 20,
+                   nboot = 10,  sgd_iters = 1000, sgd_momentum = .9, numProjections = 20, minBatch = 3, maxBatch = 20, mLearn= 0.01, dropout_rate = .5, kMatch = 3, minMatch = 2, nBoot_matching = 20,
                    verbose = F, diagnostics = F, justTransform = F, winsorize=T){ 
   
   ## Get summaries of all of the document characteristics and labeled indicator
@@ -240,18 +240,19 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   #Find E[S|D] and calculate objective function  
   ESGivenD_tf = tf$matmul(MultMat_tf,LFinal_n)
   ## Spread component of objective function
-  Spread_tf = tf$truediv(tf$sqrt(tf$maximum(tf$matmul(MultMat_tf,tf$square(LFinal_n)) - tf$square(ESGivenD_tf), 0.001)), 
-                        tf$abs(ESGivenD_tf)+0.10)
+  Spread_tf = tf$maximum(tf$matmul(MultMat_tf,tf$square(LFinal_n)) - tf$square(ESGivenD_tf), 0.001)
   ## Category discrimination (absolute difference in all E[S|D] columns)
   CatDiscrim_tf = tf$abs(tf$gather(ESGivenD_tf, indices = contrast_indices1, axis = 0L) - tf$gather(ESGivenD_tf, indices = contrast_indices2, axis = 0L))
   ## Feature discrimination (row-differences)
   FeatDiscrim_tf = tf$abs(tf$gather(CatDiscrim_tf,  indices = redund_indices1, axis = axis_FeatDiscrim) - tf$gather(CatDiscrim_tf, indices = redund_indices2, axis = axis_FeatDiscrim))
   ## Loss function CatDiscrim + FeatDiscrim + Spread_tf 
-  myLoss_tf = -(tf$reduce_mean(CatDiscrim_tf)+tf$reduce_mean(FeatDiscrim_tf) + tf$log(0.01 + Spread_tf))
+  myLoss_tf = -(tf$reduce_mean(CatDiscrim_tf)+tf$reduce_mean(FeatDiscrim_tf) + tf$log(tf$sqrt(2 * pi * exp(1) * Spread_tf)))
   #https://en.wikipedia.org/wiki/Entropic_uncertainty  
   
   ### Initialize an optimizer using stochastic gradient descent w/ momentum
-  myOptimizer_tf = tf$train$MomentumOptimizer(learning_rate=sdg_learning_rate,momentum = sgd_momentum ,use_nesterov = T)
+  myOptimizer_tf = tf$train$MomentumOptimizer(learning_rate = sdg_learning_rate,
+                                              momentum      = sgd_momentum, 
+                                              use_nesterov  = T)
   
   ### Calculates the gradients from myOptimizer_tf
   myGradients = myOptimizer_tf$compute_gradients(myLoss_tf)
@@ -315,7 +316,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
       clip_value =  0.5*median(sqrt(init_L2_squared_vec))
       
       ## Initialize vector to store learning rates
-      m1 <-inverse_learning_rate_vec <- rep(NA, times = sgd_iters) 
+      inverse_learning_rate_vec <- rep(NA, times = sgd_iters) 
       
       ## Inverse learning rate = clip value
       inverse_learning_rate <- 0.5 * median( init_L2_squared_vec ) 
