240c48d564346116bbf10eddeb83ee617304ec91
cjerzak <connor.jerzak@gmail.com>
2018-11-19 16:51:38 -0500

    #

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -158,7 +158,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   nDim                  = as.integer( ncol(dfm_labeled) )  #nDim = Number of raw features
 
   #Parameters for Batch-SGD
-  NObsPerCat            = as.integer(10)#min(r_clip_by_value(as.integer( round( sqrt(  nrow(dfm_labeled)*labeled_pd))),minBatch,maxBatch)) ## Number of observations to sample per category
+  NObsPerCat            = as.integer(5)#min(r_clip_by_value(as.integer( round( sqrt(  nrow(dfm_labeled)*labeled_pd))),minBatch,maxBatch)) ## Number of observations to sample per category
   nProj                 = as.integer(max(numProjections,nCat+1) ); ## Number of projections
   
   #Start SGD
@@ -240,10 +240,8 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   FeatDiscrim_tf       = tf$abs(tf$gather(CatDiscrim_tf,  indices = redund_indices1, axis = axis_FeatDiscrim) - tf$gather(CatDiscrim_tf, indices = redund_indices2, axis = axis_FeatDiscrim))
   
   ## Loss function CatDiscrim + FeatDiscrim + Spread_tf 
-  #myLoss_tf            = -(tf$reduce_mean(CatDiscrim_tf) + tf$reduce_mean(FeatDiscrim_tf) + tf$reduce_mean(tf$log( Spread_tf ) ) )
-  myLoss_tf            = -(  tf$reduce_mean(tf$log(tf$clip_by_value(CatDiscrim_tf,0.001, 2))) +
-                             tf$reduce_mean(tf$log(tf$clip_by_value(FeatDiscrim_tf,0.001,2))) + 
-                             tf$reduce_mean( tf$log(tf$clip_by_value(Spread_tf,0.001,2) )) )
+  myLoss_tf            = -(tf$reduce_mean(CatDiscrim_tf) + tf$reduce_mean(FeatDiscrim_tf) + tf$reduce_mean(tf$log( tf$clip_by_value(Spread_tf,0.001,1) ) ) )
+  #myLoss_tf            = -(  tf$reduce_mean(tf$log(tf$clip_by_value(CatDiscrim_tf,0.001, 2))) + tf$reduce_mean(tf$log(tf$clip_by_value(FeatDiscrim_tf,0.001,2))) + tf$reduce_mean( tf$log(tf$clip_by_value(Spread_tf,0.001,2) )) )
   
   ### Initialize an optimizer using stochastic gradient descent w/ momentum
   myOpt_tf             = tf$train$MomentumOptimizer(learning_rate = sdg_learning_rate,
