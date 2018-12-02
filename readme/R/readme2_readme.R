bd249726dc6c2af87afd1813aa45b02b5f69d1c2
cjerzak <connor.jerzak@gmail.com>
2018-12-02 12:35:01 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -364,6 +364,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
                                                   k     = kMatch)$nn.index) 
                 ## Any category with less than minMatch matches includes all of that category
                 t_              = table( Cat_[MatchIndices_i] ); t_ = t_[t_<minMatch]
+                browser()
                 if(length(t_) > 0){ for(t__ in names(t_)){MatchIndices_i = MatchIndices_i[!Cat_[MatchIndices_i] %in%  t__] ; MatchIndices_i = c(MatchIndices_i,which(Cat_ == t__ )) }}
             }else{ ## Otherwise use all the indices
                 MatchIndices_i  = 1:nrow(X_)
