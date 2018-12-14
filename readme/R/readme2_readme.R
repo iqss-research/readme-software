4ef7c1dbd3019ac6b12b1ef518c9e7f2e5570085
cjerzak <connor.jerzak@gmail.com>
2018-12-14 11:38:09 -0500

    3

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -372,6 +372,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
         } 
         ### Update the learning rate
       }
+      browser()
       ### Given the learned parameters, output the feature transformations for the entire matrix
       out_dfm           = try(sess$run(OUTPUT_LFinal,feed_dict = dict(OUTPUT_IL     = rbind(dfm_labeled, dfm_unlabeled), 
                                                                       IL_mu_last    = IL_mu_value, 
