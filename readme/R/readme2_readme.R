27dfe3a274fb907d4984eee8e996ce8f68d3a771
cjerzak <connor.jerzak@gmail.com>
2018-12-15 08:05:50 -0500

    3

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -334,7 +334,6 @@ readme <- function(dfm, labeledIndicator, categoryVec,
       inverse_learning_rate_starting = 0.50 * median( init_L2_squared_vec )
       clip_value = 0.50 * median( sqrt( init_L2_squared_vec )  )
 
-      browser() 
       sess$run(  list(clip_tf$assign(clip_value ), 
                       inverse_learning_rate$assign( inverse_learning_rate_starting ), 
                       d_1 ))
