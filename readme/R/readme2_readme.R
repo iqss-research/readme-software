ba15847ecd9eb895648833829f94c326aa227e25
cjerzak <connor.jerzak@gmail.com>
2018-12-18 17:20:05 -0500

    3

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -242,7 +242,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   OUTPUT_IL_n         = tf$nn$batch_normalization(OUTPUT_IL, mean = IL_mu_last, variance = tf$square(IL_sigma_last), offset = 0, scale = 1, variance_epsilon = 0)
   
   #SET UP WEIGHTS to be optimized
-  var(X_1*Beta_1 + ... + X_k * Beta_k) = \sum_i var(X_i) +  var(\sum_i Beta_i)
+  #var(X_1*Beta_1 + ... + X_k * Beta_k) = \sum_i var(X_i) +  var(\sum_i Beta_i)
   initializer_reweighting = 1/sd(replicate(2000, {
     beta__               = rnorm(nDim, mean = 0, sd = 1/sqrt(nDim)  )
     dropout__            = rbinom(nDim, size = 1, prob = dropout_rate)
