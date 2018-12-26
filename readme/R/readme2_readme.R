591f9fdfab3bcdb8754ab69e10fe339998896a8d
cjerzak <connor.jerzak@gmail.com>
2018-12-25 23:26:44 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -275,7 +275,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   CatDiscrim_contrib   = tf$reduce_mean(tf$minimum(CatDiscrim_tf,2)  )
   FeatDiscrim_contrib  = tf$reduce_mean(tf$minimum(FeatDiscrim_tf,2)  )
   #Spread_contrib       = 0.10*tf$reduce_mean(tf$log(tf$minimum(Spread_tf, 0.40)))
-  Spread_contrib       = 0.10*tf$reduce_mean(tf$minimum(Spread_tf, 0.40) )
+  Spread_contrib       = 0.01*tf$reduce_mean(tf$minimum(Spread_tf, 0.40) )
   myLoss_tf            = -(CatDiscrim_contrib + FeatDiscrim_contrib + Spread_contrib)
                               
   ### Initialize an optimizer using stochastic gradient descent w/ momentum
