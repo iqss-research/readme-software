4f9c25cfbc276ac84f1b61ae7808e98f946da3db
cjerzak <connor.jerzak@gmail.com>
2018-12-26 15:03:41 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -150,6 +150,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   }
   ## Drop invariant columns
   dfm                   = dfm[,apply(dfm,2,sd)>0]
+  dfm = FastScale(dfm); dfm = dfm/(1+abs(dfm))
 
   #Setup information for SGD
   categoryVec_unlabeled = as.factor( categoryVec )[labeledIndicator == 0]  
