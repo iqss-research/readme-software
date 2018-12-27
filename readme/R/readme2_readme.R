29923862812551c53e692aea003aa6096ad2d019
cjerzak <connor.jerzak@gmail.com>
2018-12-27 12:44:50 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -355,6 +355,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
                                                                                         
           MM1           = colMeans(out_dfm_unlabeled); 
           MM2_          = colSds(out_dfm_unlabeled,MM1);
+          browser() 
           BOOTSTRAP_EST = sapply(1:nboot_match, function(boot_iter){ 
             Cat_    = categoryVec_labeled[indices_list[[boot_iter]]]; 
             X_      = out_dfm_labeled[indices_list[[boot_iter]],];
