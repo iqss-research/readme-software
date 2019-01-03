8eaa406d800f1c11420658091f9fae280df4b606
cjerzak <connor.jerzak@gmail.com>
2019-01-03 17:00:03 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -340,7 +340,7 @@ readme <- function(dfm, labeledIndicator, categoryVec,
         if(rbinom(1, size = 1, prob = seq__[awer])==1){ sess$run(inverse_learning_rate$assign( inverse_learning_rate_starting )) }
         inverse_learning_rate_vec[awer] = sess$run(list(  inverse_learning_rate_update, myOpt_tf_apply,inverse_learning_rate))[[3]]
       }
-      plot(1/inverse_learning_rate_vec, main = 'That')
+      plot(1/inverse_learning_rate_vec, main = 'That and this')
       ### Given the learned parameters, output the feature transformations for the entire matrix
       out_dfm           = try(sess$run(OUTPUT_LFinal, feed_dict = dict(OUTPUT_IL     = rbind(dfm_labeled, dfm_unlabeled), 
                                                                        IL_mu_last    = IL_mu_value, 
