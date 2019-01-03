e2c96687b4e18b5a36164b79f73d723dd3690321
cjerzak <connor.jerzak@gmail.com>
2019-01-03 07:13:00 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -335,13 +335,14 @@ readme <- function(dfm, labeledIndicator, categoryVec,
       ### For each iteration of SGDs
       inverse_learning_rate_vec = rep(NA, times = sgd_iters)
       nrestart = 0
+      seq__ = seq(1, 0.01, length.out = sgd_iters)
       for(awer in 1:sgd_iters){
         #if(rbinom(1, size = 1, prob = 1/sqrt(awer))==1){ sess$run(inverse_learning_rate$assign( inverse_learning_rate_starting )) }
-        if(rbinom(1, size = 1, prob = 1/awer^0.90)==1){ sess$run(inverse_learning_rate$assign( inverse_learning_rate_starting )) }
+        if(rbinom(1, size = 1, prob = seq__[awer])==1){ sess$run(inverse_learning_rate$assign( inverse_learning_rate_starting )) }
         inverse_learning_rate_vec[awer] = sess$run(list(  inverse_learning_rate_update, myOpt_tf_apply,inverse_learning_rate))[[3]]
         #if(awer %% 5 == 0){ nrestart=nrestart+1;sess$run(inverse_learning_rate$assign( inverse_learning_rate-(1/(1+nrestart ))*inverse_learning_rate )) }
       }
-      #plot(1/inverse_learning_rate_vec, main = 'This')
+      plot(1/inverse_learning_rate_vec, main = 'This')
       ### Given the learned parameters, output the feature transformations for the entire matrix
       out_dfm           = try(sess$run(OUTPUT_LFinal, feed_dict = dict(OUTPUT_IL     = rbind(dfm_labeled, dfm_unlabeled), 
                                                                        IL_mu_last    = IL_mu_value, 
