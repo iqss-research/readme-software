fc1d92efded3a88837acfcef00a296dfe05e159d
cjerzak <connor.jerzak@gmail.com>
2019-01-04 07:28:47 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -329,18 +329,18 @@ readme <- function(dfm, labeledIndicator, categoryVec,
       inverse_learning_rate_starting = 0.50 * median( init_L2_squared_vec )
       clip_value                     = 0.50 * median( sqrt( init_L2_squared_vec )  )
 
-      sess$run(  list(clip_tf$assign(  clip_value  ), 
-                      inverse_learning_rate$assign( inverse_learning_rate_starting ) ))
+      setclip_action      = clip_tf$assign(  clip_value  )
+      warm_restart_action = inverse_learning_rate$assign( tf$constant(inverse_learning_rate_starting,dtype=tf$float32) )
+      sess$run(  list(setclip_action, 
+                      warm_restart_action ))
       
       ### For each iteration of SGDs
-      warm_restart_action = inverse_learning_rate$assign( tf$constant(inverse_learning_rate_starting,dtype=tf$float32) )
       inverse_learning_rate_vec = rep(NA, times = sgd_iters)
       seq__ = seq(1, 0.01, length.out = sgd_iters)^10
       #seq__ = seq__ / sum(seq__) * (sgd_iters*0.01)
       seq__ = seq__ / sum(seq__) * (100);seq__[seq__>0.50] <- 0.50
       for(awer in 1:sgd_iters){
-        if(rbinom(1, size = 1, 
-                  prob = seq__[awer])==1){ sess$run(warm_restart_action) }
+        if(rbinom(1, size = 1, prob = seq__[awer])==1){ sess$run(warm_restart_action) }
         inverse_learning_rate_vec[awer] = sess$run(list(  inverse_learning_rate_update, myOpt_tf_apply,inverse_learning_rate))[[3]]
       }
       ### Given the learned parameters, output the feature transformations for the entire matrix
