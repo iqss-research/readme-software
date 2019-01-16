3f543c0028931c7b0366a7e0472544c674268958
cjerzak <connor.jerzak@gmail.com>
2019-01-16 13:39:25 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -214,15 +214,17 @@ readme <- function(dfm, labeledIndicator, categoryVec,
     
   #SET UP INPUT layer to TensorFlow and apply batch normalization for the input layer
   for(ape in 1:nCat){ 
-      #eval(parse(text = sprintf("d_%s = tf$data$Dataset$from_tensor_slices(tf$convert_to_tensor(dfm_labeled[l_indices_by_cat[[ape]],], dtype = tf$float32))$`repeat`()$shuffle(as.integer(min(1000,
-                                #length(l_indices_by_cat[[ape]])+1)))$batch(NObsPerCat)$prefetch(buffer_size = 1L)", ape)) )
-      eval(parse(text = sprintf("d_%s = tf$data$Dataset$from_tensor_slices(dfm_labeled[l_indices_by_cat[[ape]],])$`repeat`()$shuffle(as.integer(min(1000,
-                                            length(l_indices_by_cat[[ape]])+1)))$batch(NObsPerCat)$prefetch(buffer_size = 1L)", ape)) )
+      eval(parse(text = sprintf("d_%s = tf$data$Dataset$from_tensor_slices(tf$convert_to_tensor(dfm_labeled[l_indices_by_cat[[ape]],], dtype = tf$float32))$`repeat`()$shuffle(as.integer(min(1000,
+                                length(l_indices_by_cat[[ape]])+1)))$batch(NObsPerCat)$prefetch(buffer_size = 1L)", ape)) )
+      #eval(parse(text = sprintf("d_%s = tf$data$Dataset$from_tensor_slices(dfm_labeled[l_indices_by_cat[[ape]],])$`repeat`()$shuffle(as.integer(min(1000,
+                                            #length(l_indices_by_cat[[ape]])+1)))$batch(NObsPerCat)$prefetch(buffer_size = 1L)", ape)) )
       eval(parse(text = sprintf("b_%s = d_%s$make_one_shot_iterator()$get_next()", ape,ape)) )
   } 
-    IL_input            = eval(parse(text = sprintf("tf$cast(tf$reshape(tf$concat(list(%s), 0L), 
-                                                    list(as.integer(nCat*NObsPerCat),nDim)), tf$float32)", 
-                                                    paste(paste("b_", 1:nCat, sep = ""), collapse = ","))))
+    #IL_input            = eval(parse(text = sprintf("tf$cast(tf$reshape(tf$concat(list(%s), 0L), 
+                                                    #list(as.integer(nCat*NObsPerCat),nDim)), tf$float32)", 
+                                                    #paste(paste("b_", 1:nCat, sep = ""), collapse = ","))))
+  IL_input            = eval(parse(text = sprintf("tf$reshape(tf$concat(list(%s), 0L), 
+  list(as.integer(nCat*NObsPerCat),nDim))", paste(paste("b_", 1:nCat, sep = ""), collapse = ","))))
   IL_m                = tf$nn$moments(IL_input, axes = 0L);
   IL_mu_b             = IL_m[[1]];
   IL_sigma2_b         = IL_m[[2]];
