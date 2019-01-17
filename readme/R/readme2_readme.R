f8da84f0d1f98ad5bb6cbef7fb4dff063893a185
cjerzak <connor.jerzak@gmail.com>
2019-01-17 14:50:28 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -177,7 +177,9 @@ readme <- function(dfm, labeledIndicator, categoryVec,
   }
   # Initialize tensorflow
   tf$reset_default_graph(); 
-  gpu_options = tf$GPUOptions(per_process_gpu_memory_fraction = 0.333)
+  #gpu_options = tf$GPUOptions(per_process_gpu_memory_fraction = 0.333)
+  browser()
+  config$gpu_options$allow_growth=True
   sess <- tf$Session(config=tf$ConfigProto(gpu_options=gpu_options))
   
   ## Construct TensorFlow graph
