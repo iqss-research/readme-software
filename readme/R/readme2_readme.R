54300d6b5da25a13d60ee19a4f2ea4c9ada2f98d
cjerzak <connor.jerzak@gmail.com>
2019-01-18 16:20:27 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -162,7 +162,7 @@ readme <- function(dfm,
   tf$reset_default_graph(); 
   #gpu_options = tf$GPUOptions(allow_growth = T)
   #sess <- tf$Session(config=tf$ConfigProto(gpu_options=gpu_options))
-  sess <- tf$Session(graph = tf$Graph())
+  sess <- tf$Session()
   
   ## Construct TensorFlow graph
   if (verbose == T){
