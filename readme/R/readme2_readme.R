9a953a6dcaead2831b8c4a0fd8bb07b63d29886c
cjerzak <connor.jerzak@gmail.com>
2019-01-20 12:06:55 -0500

    E

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -164,6 +164,7 @@ readme <- function(dfm,
   tf$reset_default_graph(); 
   #gpu_options = tf$GPUOptions(allow_growth = T)
   #sess <- tf$Session(config=tf$ConfigProto(gpu_options=gpu_options))
+  browser()
   sess <- tf$Session(graph = tf$get_default_graph(), 
                      config = tf$ConfigProto(
                        allow_soft_placement = TRUE, 
