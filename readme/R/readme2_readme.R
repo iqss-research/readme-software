fd39996bf27a7ef34340eb1c692f9204da7e0493
cjerzak <connor.jerzak@gmail.com>
2019-01-24 20:22:08 -0500

    e

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -158,12 +158,18 @@ readme <- function(dfm = NULL,
   # Initialize tensorflow
   FinalParams_LIST <- list() 
   tf_junk <- ls()
-  try(detach("package:tensorflow", unload=TRUE), T)  
-  try(detach("package:reticulate", unload=TRUE), T)  
-  require("tensorflow", quietly = T)
+  #try(detach("package:tensorflow", unload=TRUE), T)  
+  #require("tensorflow", quietly = T)
   tf$reset_default_graph()
   G_ = tf$Graph()
-  with(G_$as_default(), {
+  with(c(G_$as_default(), 
+       tf$Session(graph = G_,
+                  config = tf$ConfigProto(
+                    allow_soft_placement = TRUE 
+                    #device_count=list("GPU"=0L, "CPU" = nCores), 
+                    #inter_op_parallelism_threads = nCores,intra_op_parallelism_threads = nCores
+                  )) %as% sess)
+       , {
   ## For calculating discrimination - how many possible cross-category contrasts are there
   contrasts_mat       = combn(1:nCat, 2) - 1
   contrast_indices1   = as.integer(contrasts_mat[1,])
@@ -281,14 +287,7 @@ readme <- function(dfm = NULL,
   L2_squared_initial      = tf$placeholder(tf$float32)
   setclip_action          = clip_tf$assign(  0.50 * sqrt( L2_squared_initial )  )
   restart_action          = inverse_learning_rate$assign(  0.50 *  L2_squared_initial )
-  } ) 
-  G_$finalize(); 
-  with(tf$Session(graph = G_,
-                     config = tf$ConfigProto(
-                       allow_soft_placement = TRUE 
-                       #device_count=list("GPU"=0L, "CPU" = nCores), 
-                       #inter_op_parallelism_threads = nCores,intra_op_parallelism_threads = nCores
-                     )) %as% sess, { 
+  
       for(iter_i in 1:nboot){ 
       sess$run(init) # Initialize TensorFlow graph
       ## Print iteration count
@@ -317,8 +316,9 @@ readme <- function(dfm = NULL,
 }
       try(sess$close(), T) 
       try(tf$keras$backend$clear(), T)  
-  })  
+  })
   
+  G_ = tf$Graph()
   tf$reset_default_graph()
   tf_junk <- ls()[!ls() %in% c(tf_junk, "IL_mu_last_v","IL_sigma_last_v" )]
   eval(parse(text = sprintf("rm(%s)", paste(tf_junk, collapse = ","))))
