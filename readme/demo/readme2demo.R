1ac6cf0629b4474e78890c0cd8f43b832450ea0f
Anton Strezhnev <a.strezhnev@gmail.com>
2019-02-08 13:09:26 -0500

    Updated vignette

---
+++ readme/demo/readme2demo.R
@@ -0,0 +1,26 @@
+##########
+### An example for how to generate estimates using readme2
+##########
+
+## Installation
+library(readme)
+library(tensorflow)
+
+## Load the data
+data(clinton, package="readme")
+
+## Load in the word vectors
+download_wordvecs()
+
+## Generate a word vector summary for each document using the cleaned text
+word_vectors = undergrad(documentText = cleanme(clinton$TEXT), wordVecs = NULL)
+
+# Estimate category proportions
+set.seed(2138) # Set a seed
+readme.estimates <- readme(dfm = as.matrix(word_vectors) , labeledIndicator = clinton$TRAININGSET, categoryVec = clinton$TRUTH)
+
+# Output proportions estimate
+readme.estimates$point_readme
+
+# Compare to the truth
+table(clinton$TRUTH[clinton$TRAININGSET == 0])/sum(clinton$TRUTH[clinton$TRAININGSET == 0])
