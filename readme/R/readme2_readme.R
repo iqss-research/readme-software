d6d6fcb721b10b28737ca69c4cec72fc926b4af6
cjerzak <connor.jerzak@gmail.com>
2019-07-22 13:52:37 -0400

    test

--- readme/R/readme2_readme.R
+++ readme/R/readme2_readme.R
@@ -697,8 +697,8 @@ start_reading <- function(nDim,bagFrac = 1, nProj=20,regraph = F){
     #other actions 
     FinalParams_list        = list(WtsMat, BiasVec)
     setclip_action          = clip_tf$assign(  0.50 * sqrt( L2_squared_initial )  )
-    #restart_action          = list(WtsMat, BiasVec,inverse_learning_rate$assign(  0.50 *  L2_squared_initial ))
-    restart_action          = list(tf$variables_initializer(list(WtsMat, BiasVec)),inverse_learning_rate$assign(  0.50 *  L2_squared_initial ))
+    restart_action          = inverse_learning_rate$assign(  0.50 *  L2_squared_initial )
+    #restart_action          = list(tf$variables_initializer(list(WtsMat, BiasVec)),inverse_learning_rate$assign(  0.50 *  L2_squared_initial ))
   })
   readme_graph$finalize()
 
